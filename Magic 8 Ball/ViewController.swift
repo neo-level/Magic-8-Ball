b9aa1a4b0c8d249bd9550e982c0abe304ef03f87
neo-level <frederik@neo-level.com>
2023-07-16 21:41:17 +0200

    finalised challenge by creating a design eg. adding text, image view and button then by adding a reference to the image view in my view controller. added a action for the button. Finished by creating a random ball function that randomly gets an image from the array and displays it.

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var ImageViewBall: UIImageView!
+    
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
 
 
-
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        ImageViewBall.image = getBall()
+    }
+    
+    func getBall() -> UIImage?  {
+        if let image = ballArray.randomElement() {
+            return image
+        }
+        return UIImage();
+    }
 }
 
